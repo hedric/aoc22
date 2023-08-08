ea3a9fabac3585c9998d548f96fb4650250cbb1d
Richard Hedlund <richard.hedlund@gmail.com>
2023-08-08 06:50:03 +0200

    Add initial day1 testing

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+//use std::fs;
+fn main(){
+    println!("AoC 2022, day1");
+
+    // let s = fs::read_to_string("input.txt")
+    // .expect("Should have been able to read the file!");
+
+    let s = String::from("1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000");
+
+    let lines: Vec <_> = s.lines().map(String::from).collect();
+    
+    println!{"{:?}",lines}
 }
